0dd4215d7800868aebdc44580d0beb9c0fbd9a0a
Andreas Haugstrup Pedersen <haugstrup@podio.com>
2014-07-08 07:44:59 -0700

    Remove seeders

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -13,20 +13,20 @@ class DatabaseSeeder extends Seeder {
 
 		DB::statement('SET FOREIGN_KEY_CHECKS=0;');
 
-		$this->call('UsersTableSeeder');
-		$this->command->info('Users table seeded.');
+		// $this->call('UsersTableSeeder');
+		// $this->command->info('Users table seeded.');
 
-		$this->call('PlayersTableSeeder');
-		$this->command->info('Players table seeded.');
+		// $this->call('PlayersTableSeeder');
+		// $this->command->info('Players table seeded.');
 
-		$this->call('SeasonsTableSeeder');
-		$this->command->info('Seasons table seeded.');
+		// $this->call('SeasonsTableSeeder');
+		// $this->command->info('Seasons table seeded.');
 
-		$this->call('MachinesTableSeeder');
-		$this->command->info('Machines table seeded.');
+		// $this->call('MachinesTableSeeder');
+		// $this->command->info('Machines table seeded.');
 
-		$this->call('HeatsTableSeeder');
-		$this->command->info('Heats table seeded.');
+		// $this->call('HeatsTableSeeder');
+		// $this->command->info('Heats table seeded.');
 
 		DB::statement('SET FOREIGN_KEY_CHECKS=1;');
 	}
