c4933e4be224961e60b565b426f7ab552629ef8a
Andreas Haugstrup Pedersen <haugstrup@podio.com>
2014-07-07 19:10:21 -0700

    Migration for activity log table

---
+++ app/database/migrations/2014_07_07_184921_create_logs_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLogsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('activity', function(Blueprint $table)
+		{
+			$table->increments('activity_id');
+			$table->string('ref_type');
+			$table->integer('ref_id')->unsigned()->nullable();
+			$table->string('action');
+			$table->string('data');
+			$table->timestamps();
+			$table->softDeletes();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('activity');
+	}
+
+}
