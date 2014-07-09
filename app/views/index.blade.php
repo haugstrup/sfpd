1c4cca9817ef9cb057564eab6ac49ff8518ad2e7
Andreas Haugstrup Pedersen <haugstrup@podio.com>
2014-07-08 19:58:01 -0700

    Ajax loading indicator for requests to /api/

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -16,8 +16,11 @@
             <a class="navbar-brand" href="/"><img src="/img/logo64.png" alt="SFPD" width="32" height="32"></a>
             <ul class="nav navbar-nav">
               <li ng-class="{ active: isActive('/standings')}"><a ng-href="#/standings">Standings</a></li>
-              <li  ng-class="{ active: isActive('/results')}"><a ng-href="#/results">Results</a></li>
+              <li ng-class="{ active: isActive('/results')}"><a ng-href="#/results">Results</a></li>
             </ul>
+          </div>
+          <div class="navbar-right">
+            <div loading-indicator></div>
           </div>
         </div>
 
@@ -28,6 +31,7 @@
         <div class="footer text-muted">
           <p><small>Comments or complaints? <a target="_blank" href="https://podio.com/webforms/8656053/642967">Get in touch</a>.</small></p>
         </div>
+
     </div>
 
     <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.2.19/angular.min.js"></script>
--- public/js/app.js
+++ public/js/app.js
@@ -6,7 +6,7 @@ angular
     'groupService',
     'gameService'
   ])
-  .config(function ($routeProvider) {
+  .config(function ($routeProvider, $httpProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'public/views/main.html',
@@ -51,4 +51,42 @@ angular
       .otherwise({
         redirectTo: '/'
       });
+
+    $httpProvider.interceptors.push(function($q, $rootScope) {
+      var count = 0;
+      return {
+        'request': function(config) {
+          if (config.url.indexOf('/api/') !== -1) {
+            count++;
+            $rootScope.$broadcast('loading-started');
+          }
+          return config || $q.when(config);
+        },
+        'response': function(response) {
+          if (response.config.url.indexOf('/api/') !== -1) {
+            count--;
+            if (count <= 0) {
+              $rootScope.$broadcast('loading-complete');
+            }
+          }
+          return response || $q.when(response);
+        }
+      };
+    });
+
+  })
+  .directive("loadingIndicator", function() {
+    return {
+      restrict : "A",
+      template: "<div class='loading-indicator'><div class='bar-1'></div><div class='bar-2'></div><div class='bar-3'></div><div class='bar-4'></div><div class='bar-5'></div><div class='bar-6'></div><div class='bar-7'></div><div class='bar-8'></div></div>",
+      link : function(scope, element, attrs) {
+        scope.$on("loading-started", function(e) {
+          element.css({"display" : ""});
+        });
+
+        scope.$on("loading-complete", function(e) {
+          element.css({"display" : "none"});
+        });
+      }
+    };
   });
