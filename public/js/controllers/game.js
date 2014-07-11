35999f3fb71eaf7a51e345165c3879cef6bd5a66
Andreas Haugstrup Pedersen <haugstrup@podio.com>
2014-07-11 08:06:48 -0700

    Disable store results button until results are valid

--- public/js/controllers/game.js
+++ public/js/controllers/game.js
@@ -2,13 +2,14 @@ angular.module('sfpdApp')
   .controller('GameCtrl', function ($scope, $location, game) {
     $scope.game = game;
     $scope.removeButtonDisabled = false;
-    $scope.saveButtonDisabled = false;
+    $scope.saveButtonDisabledState = false;
 
     $scope.positions = [
       {id: 1, title: '1st'},
       {id: 2, title: '2nd'},
       {id: 3, title: '3rd'},
       {id: 4, title: '4th'},
+      {id: 0, title: 'D.Q.'},
       {id: -1, title: 'Tardy player'}
     ];
 
@@ -32,8 +33,11 @@ angular.module('sfpdApp')
       });
     };
 
-    $scope.storeResults = function() {
+    $scope.saveButtonDisabled = function() {
+      return $scope.saveButtonDisabledState || !$scope.resultsValid();
+    };
 
+    $scope.resultsValid = function() {
       // Make sure that each position is only used once
       var usedPositions = [];
       var resultsValid = true;
@@ -56,12 +60,17 @@ angular.module('sfpdApp')
         resultsValid = true;
       }
 
-      if (!resultsValid) {
+      return resultsValid;
+    };
+
+    $scope.storeResults = function() {
+
+      if (!$scope.resultsValid()) {
         return;
       }
 
       // Mark game as completed and submit to API
-      $scope.saveButtonDisabled = true;
+      $scope.saveButtonDisabledState = true;
       $scope.game.status = 'completed';
       $scope.game.$update(function() {
         $location.path('/group/'+game.group.code);
