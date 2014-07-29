15abfd718b6d13546455101f45139db464315bf4
Andreas Haugstrup Pedersen <haugstrup@podio.com>
2014-07-28 18:52:38 -0700

    Use finishing positions for printed sheets and adjust margins

--- app/views/heats/print.blade.php
+++ app/views/heats/print.blade.php
@@ -5,13 +5,13 @@
   <div class="sheet">
     <div class="mobile">
       {{ QrCode::size(100)->generate($group->url()); }}<br>
-      Code:<br><b>{{{$group->code}}}</b>
+      Code:<br><b class="code">{{{$group->code}}}</b>
     </div>
     <div class="scores">
       <table>
         <thead>
           <tr class="fake-header">
-            <th colspan="3">SFPD Score Sheet, {{{$group->name()}}}</th>
+            <th colspan="3"><img src="/img/ball-19.png" alt="" width="18" height="19">SFPD Score Sheet, {{{$group->name()}}}</th>
             <th colspan="3" class="name">{{{$heat->season->name}}}, {{{$heat->name()}}}</th>
           </tr>
           <tr>
@@ -20,46 +20,58 @@
             <th>Pin:</th>
             <th>Pin:</th>
             <th>Pin:</th>
-            <th class="total">Total</th>
           </tr>
         </thead>
         <tbody>
+          @for ($i = 0; $i < 4; $i++)
             <tr>
               <td>&nbsp;</td>
-              <td>&nbsp;</td>
-              <td>&nbsp;</td>
-              <td>&nbsp;</td>
-              <td>&nbsp;</td>
-              <td>&nbsp;</td>
+              <td>
+                <table class="positions">
+                  <tr>
+                    <td class="small">1st</td>
+                    <td class="small">2nd</td>
+                    <td class="small">3rd</td>
+                    <td class="small">4th</td>
+                  </tr>
+                </table>
+              </td>
+              <td>
+                <table class="positions">
+                  <tr>
+                    <td class="small">1st</td>
+                    <td class="small">2nd</td>
+                    <td class="small">3rd</td>
+                    <td class="small">4th</td>
+                  </tr>
+                </table>
+              </td>
+              <td>
+                <table class="positions">
+                  <tr>
+                    <td class="small">1st</td>
+                    <td class="small">2nd</td>
+                    <td class="small">3rd</td>
+                    <td class="small">4th</td>
+                  </tr>
+                </table>
+              </td>
+              <td>
+                <table class="positions">
+                  <tr>
+                    <td class="small">1st</td>
+                    <td class="small">2nd</td>
+                    <td class="small">3rd</td>
+                    <td class="small">4th</td>
+                  </tr>
+                </table>
+              </td>
             </tr>
-            <tr>
-              <td>&nbsp;</td>
-              <td>&nbsp;</td>
-              <td>&nbsp;</td>
-              <td>&nbsp;</td>
-              <td>&nbsp;</td>
-              <td>&nbsp;</td>
-            </tr>
-            <tr>
-              <td>&nbsp;</td>
-              <td>&nbsp;</td>
-              <td>&nbsp;</td>
-              <td>&nbsp;</td>
-              <td>&nbsp;</td>
-              <td>&nbsp;</td>
-            </tr>
-            <tr>
-              <td>&nbsp;</td>
-              <td>&nbsp;</td>
-              <td>&nbsp;</td>
-              <td>&nbsp;</td>
-              <td>&nbsp;</td>
-              <td>&nbsp;</td>
-            </tr>
+          @endfor
         </tbody>
         <tfoot>
           <tr>
-            <td colspan="6">Fill in the <b>points</b> each player earned. Use the mobile app: <b>{{url('/')}}</b></td>
+            <td colspan="6" class="small">Circle the <b>finishing position</b> for each player. Use the mobile app: <b>{{url('/')}}</b></td>
           </tr>
         </tfoot>
       </table>
