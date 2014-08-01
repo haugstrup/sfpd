d3b7666cb11e4a1323064b1b31d3760db00c4f63
Andreas Haugstrup Pedersen <haugstrup@podio.com>
2014-07-31 21:54:15 -0700

    Only grab latest 100 activities for the public log

--- app/controllers/AdminActivityController.php
+++ app/controllers/AdminActivityController.php
@@ -4,7 +4,7 @@ class AdminActivityController extends \BaseController {
 
   public function index()
   {
-    $activities = Activity::orderBy('created_at', 'desc')->get();
+    $activities = Activity::take(100)->orderBy('created_at', 'desc')->get();
     return View::make('activities.index', array('activities' => $activities));
   }
 
