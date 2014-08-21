e595563ef5e472f605ff00d6bb86a0b55278b4b7
Andreas Haugstrup Pedersen <haugstrup@podio.com>
2014-08-21 09:40:01 -0700

    Fix percentage counts

--- app/controllers/AdminStatsController.php
+++ app/controllers/AdminStatsController.php
@@ -24,7 +24,6 @@ class AdminStatsController extends \BaseController {
 
     // Popularity by type
     $type_popular = DB::table('games')
-      ->select(array('machines.type', DB::raw('COUNT(*) as aggregate'), DB::raw('ROUND(COUNT(*) / (SELECT COUNT(*) FROM games) * 100, 2) AS percentage')))
       ->where('games.deleted_at', null)
       ->join('machines', 'games.machine_id', '=', 'machines.machine_id')
       ->groupBy('machines.type')
@@ -47,14 +46,19 @@ class AdminStatsController extends \BaseController {
       if ($current_filter[0] === 'heat') {
         $type_popular->where('groups.heat_id', $current_filter[1]);
         $list_popular->where('groups.heat_id', $current_filter[1]);
+
+        $type_popular->select(array('machines.type', DB::raw('COUNT(*) as aggregate'), DB::raw('ROUND(COUNT(*) / (SELECT COUNT(*) FROM games INNER JOIN groups ON groups.group_id = games.group_id WHERE groups.heat_id = '.DB::connection()->getPdo()->quote($current_filter[1]).') * 100, 2) AS percentage')));
       }
       elseif ($current_filter[0] === 'season') {
         $type_popular->join('heats', 'heats.heat_id', '=', 'groups.heat_id');
         $type_popular->where('heats.season_id', $current_filter[1]);
         $list_popular->join('heats', 'heats.heat_id', '=', 'groups.heat_id');
         $list_popular->where('heats.season_id', $current_filter[1]);
-      }
 
+        $type_popular->select(array('machines.type', DB::raw('COUNT(*) as aggregate'), DB::raw('ROUND(COUNT(*) / (SELECT COUNT(*) FROM games INNER JOIN groups ON groups.group_id = games.group_id INNER JOIN heats ON heats.heat_id = groups.heat_id WHERE heats.season_id = '.DB::connection()->getPdo()->quote($current_filter[1]).') * 100, 2) AS percentage')));
+      }
+    } else {
+      $type_popular->select(array('machines.type', DB::raw('COUNT(*) as aggregate'), DB::raw('ROUND(COUNT(*) / (SELECT COUNT(*) FROM games) * 100, 2) AS percentage')));
     }
 
 
