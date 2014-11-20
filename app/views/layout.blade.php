1aa884071efefeb3a80e37735389093b3293768b
Andreas Haugstrup Pedersen <haugstrup@podio.com>
2014-11-19 20:57:24 -0800

    Bring navbar back to one line

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -9,40 +9,34 @@
 </head>
 <body>
 
-  <div class="container">
+  @if (Auth::check())
+  <div class="navbar navbar-default navbar-static-top">
+    <div class="navbar-inner">
+      <!-- <a class="navbar-brand" href="{{ URL::route('admin.index') }}"><img src="/img/logo64.png" alt="SFPD" width="32" height="32"></a> -->
+      <ul class="nav navbar-nav">
 
-    @if (Auth::check())
-    <div class="navbar navbar-default">
-      <div class="navbar-inner">
-        <a class="navbar-brand" href="{{ URL::route('admin.index') }}"><img src="/img/logo64.png" alt="SFPD" width="32" height="32"></a>
-        <ul class="nav navbar-nav">
+        <li class="{{ substr(Request::path(), 6, 7) == 'players' ? 'active' : '' }}">
+          <a href="{{ URL::route('admin.players.index') }}">Players</a>
+        </li>
 
-          <li class="{{ substr(Request::path(), 6, 7) == 'players' ? 'active' : '' }}">
-            <a href="{{ URL::route('admin.players.index') }}">Players</a>
-          </li>
+        <li class="{{ substr(Request::path(), 6, 8) == 'machines' ? 'active' : '' }}">
+          <a href="{{ URL::route('admin.machines.index') }}">Pins</a>
+        </li>
 
-          <li class="{{ substr(Request::path(), 6, 8) == 'machines' ? 'active' : '' }}">
-            <a href="{{ URL::route('admin.machines.index') }}">Pins</a>
-          </li>
+        <li class="{{ substr(Request::path(), 6, 7) == 'seasons' ? 'active' : '' }}">
+          <a href="{{ URL::route('admin.seasons.index') }}">Seasons</a>
+        </li>
 
-          <li>
-            <a href="{{ URL::route('admin.heats.current') }}" title="View current round"><span class="glyphicon glyphicon-dashboard"></span></a>
-          </li>
-
-          <li class="{{ substr(Request::path(), 6, 5) == 'stats' ? 'active' : '' }}">
-            <a href="{{ URL::route('admin.stats.machines') }}" title="Stats"><span class="glyphicon glyphicon-stats"></span></a>
-          </li>
-
-          <li class="{{ substr(Request::path(), 6, 7) == 'seasons' ? 'active' : '' }}">
-            <a href="{{ URL::route('admin.seasons.index') }}" title="Seasons"><span class="glyphicon glyphicon-calendar"></span></a>
-          </li>
-
-          <li><a href="{{ URL::route('logout') }}" title="Log out"><span class="glyphicon glyphicon-log-out"></span></a></li>
-        </ul>
-      </div>
+        <li class="{{ substr(Request::path(), 6, 5) == 'stats' ? 'active' : '' }}">
+          <a href="{{ URL::route('admin.stats.machines') }}">Stats</a>
+        </li>
+        <li><a href="{{ URL::route('logout') }}"><span class="glyphicon glyphicon-log-out"></span></a></li>
+      </ul>
     </div>
-    @endif
+  </div>
+  @endif
 
+  <div class="container">
     @if(Session::get('success'))<div class="alert alert-success">{{ Session::get('success') }}</div>@endif
     @if(Session::get('error'))<div class="alert alert-danger">{{ Session::get('error') }}</div>@endif
     @if(Session::get('errors'))
