9708419777a76cb1453d886c50f3fb8cb9ae5865
Andreas Haugstrup Pedersen <haugstrup@podio.com>
2015-01-04 17:47:35 -0800

    Make scoresheets display with 5 games per round

--- app/views/heats/print.blade.php
+++ app/views/heats/print.blade.php
@@ -3,29 +3,29 @@
 @if ($heat->groups)
   @foreach ($heat->groups as $group)
   <div class="sheet">
-    <div class="mobile">
-      {{ QrCode::size(100)->generate($group->url()); }}<br>
-      Code:<br><b class="code">{{{$group->code}}}</b>
-    </div>
     <div class="scores">
+      <div class="code-cell">
+        Code:
+        <b>{{{$group->code}}}</b>
+      </div>
       <table>
         <thead>
           <tr class="fake-header">
-            <th colspan="4"><img src="/img/ball-19.png" alt="" width="18" height="19">{{{$heat->season->name}}}, {{{$heat->name()}}}</th>
+            <th colspan="4">SFPD {{{$heat->season->name}}}, {{{$heat->name()}}}</th>
             <th colspan="2" class="name">{{{$group->name()}}}</th>
           </tr>
           <tr>
-            <th>Player Name</th>
-            <th>Pin:</th>
-            <th>Pin:</th>
-            <th>Pin:</th>
-            <th>Pin:</th>
+            <th class="player-name">Player</th>
+            @for ($i = 0; $i < $heat->season->game_count; $i++)
+              <th>Pin:</th>
+            @endfor
           </tr>
         </thead>
         <tbody>
           @for ($i = 0; $i < 4; $i++)
             <tr>
-              <td>&nbsp;</td>
+              <td class="player-name">&nbsp;</td>
+              @for ($j = 0; $j < $heat->season->game_count; $j++)
               <td>
                 <table class="positions">
                   <tr>
@@ -36,42 +36,13 @@
                   </tr>
                 </table>
               </td>
-              <td>
-                <table class="positions">
-                  <tr>
-                    <td class="small">1st</td>
-                    <td class="small">2nd</td>
-                    <td class="small">3rd</td>
-                    <td class="small">4th</td>
-                  </tr>
-                </table>
-              </td>
-              <td>
-                <table class="positions">
-                  <tr>
-                    <td class="small">1st</td>
-                    <td class="small">2nd</td>
-                    <td class="small">3rd</td>
-                    <td class="small">4th</td>
-                  </tr>
-                </table>
-              </td>
-              <td>
-                <table class="positions">
-                  <tr>
-                    <td class="small">1st</td>
-                    <td class="small">2nd</td>
-                    <td class="small">3rd</td>
-                    <td class="small">4th</td>
-                  </tr>
-                </table>
-              </td>
+              @endfor
             </tr>
           @endfor
         </tbody>
         <tfoot>
           <tr>
-            <td colspan="6" class="small">Circle the <b>finishing position</b> for each player. Use the mobile app: <b>{{url('/')}}</b></td>
+            <td colspan="5" class="small">Circle the <b>finishing position</b> for each player. Use the mobile app: <b>{{url('/')}}</b></td>
           </tr>
         </tfoot>
       </table>
