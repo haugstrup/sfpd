88cbf338a336f3021610f9834f4fe7cd74b5012f
Andreas Haugstrup Pedersen <andreas@solitude.dk>
2015-09-03 07:59:32 -0700

    Add permaban list

--- app/views/public/stats/notes.blade.php
+++ app/views/public/stats/notes.blade.php
@@ -5,6 +5,23 @@
 @section("content")
 <h2>Notes</h2>
 <p>Find below notes about why a game was removed in a given round</p>
+<h3>Permanently out</h3>
+<div>
+  Bride of Pinbot (unsuitable rules)<br>
+  <!-- Police Force (unsuitable rules)<br> -->
+  Super Mario Bros (unsuitable rules)<br>
+  Super Mario Bros. Mushroom World (unfair to tall people)<br>
+  <!-- Black Knight (always breaks down)<br> -->
+  Black Knight 2000 (always breaks down)<br>
+  Captain Fantastic (playfield protector catches ball)<br>
+  <!-- Checkpoint (display always goes out)<br> -->
+  <!-- Cyclone (scoop kicks ball SDTM)<br> -->
+  <!-- Fireball 2 (no tilt bob)<br> -->
+  High-Speed (no tilt bob)<br>
+  <!-- Popeye (confuses locks)<br> -->
+  Terminator 3 (ends balls prematurely)
+  <!-- Tommy (multiball problems) -->
+</div>
 @foreach ($heats as $heat)
 <h3>{{{$heat->season->name}}}, {{{$heat->name}}}</h3>
 <div>
